b2e95e448e2aacab29d62cfd1a1d5e95ed7f6228
Elagost <adamj@mailbox.org>
2022-04-08 08:56:37 -0500

    script update

--- battery-limit.sh
+++ battery-limit.sh
@@ -1,16 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 #default options
 BAT='BAT0'
 LIMIT='95'
-
-while getopts 'hb:l:' opt
-do
-  case $opt in
-    h) helptext ;;
-    b) BAT=$OPTARG ;;
-    l) LIMIT=$OPTARG ;;
-  esac
-done
+BC=0
+LC=0
+AVAIL_BATS=$(cd /sys/class/power_supply/ && ls -d BAT*)
 
 helptext() {
   echo -e "
@@ -19,22 +13,45 @@ USAGE: battery-limit.sh [OPTIONS]
 OPTIONS:
   -h - OPTIONAL - show this help text.
   -b - OPTIONAL - battery to use from /sys/class/power_supply/. Default is BAT0.
-  -l - OPTIONAL - limit percentage. Default is 95.
+       Choose from the following: ${AVAIL_BATS}
+  -l - OPTIONAL - limit percentage between 1-99. Default is 95.
   "
 }
 
-check() {
-  if [[ -d /sys/class/power_supply/ && -d /sys/class/power_supply/BAT0 ]];then
+while getopts 'hb:l:' opt
+do
+  case $opt in
+    h) helptext ;;
+    b) BAT=$OPTARG BC='1' ;;
+    l) LIMIT=$OPTARG LC='1' ;;
+  esac
+done
+
+validate() {
+  if [[ -d /sys/class/power_supply/ ]];then
     echo "Platform has battery power supply, continue"
   else
     echo "Platform does not have a battery."
     exit 1
   fi
+  if [[ -d /sys/class/power_supply/${BAT} ]];then
+    echo "Battery ${BAT} exists."
+  else
+    echo "
+    Battery ${BAT} does not exist.
+    Choose from the available batteries:
+    ${AVAIL_BATS}
+    "
+  fi
+  if [[ ${LIMIT} -gt "99" ]];then
+    echo "Limit should be 
 }
 
 show(){
-  echo "chosen battery: ${BAT}"
-  echo "chosen limit: ${LIMIT}"
+  echo "battery: ${BAT} ${BC}"
+  echo "limit: ${LIMIT} ${LC}"
 }
 
+# Run!
+validate
 show
